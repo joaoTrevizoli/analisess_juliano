bde5fecb1f1960906db439296a288e10d09bd9c9
João Trevizoli Esteves <joao@lab804.com.br>
2021-05-02 23:46:28 -0300

    passos do software

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# TODO: Converter os arquivos em csv
+# TODO: Definir um score de maturação
+# TODO: Montar um gráfico de dispersão
+# TODO: Teste de média entre os grupos verietais e entre os locais.
+# TODO: Definir se vai ser um modelo linear ou nao linear.
+# TODO: Testar os ajustes dos modelos, com (Temperatura, chuva, irradiação, umidade relativa)
+# TODO: Definir melhor modelo
+# TODO: Validação cruzada
+# TODO: Explica o modelo
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
